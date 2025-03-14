9a842c59ebb40824cedf2b54b37ef6eb929a200d
Aaron Wright <aaron@sikosoft.com>
2025-03-14 16:24:04 +0100

    implement access middleware layer

--- src/server/middleware/access.ts
+++ src/server/middleware/access.ts
@@ -1,3 +1,24 @@
-export default function () {
+import { MiddlewarePayload, MiddlewareTrigger } from "../lib/Middleware";
+import { App } from "../models/App";
+
+export default function (app: App) {
   console.log("access middleware entry...");
+  app.middleware.register(
+    MiddlewareTrigger.CLIENT_CONNECTED,
+    async (
+      payload: MiddlewarePayload[MiddlewareTrigger.CLIENT_CONNECTED]
+    ): Promise<MiddlewarePayload[MiddlewareTrigger.CLIENT_CONNECTED]> => {
+      app.logger.info("client connected middleware handler!");
+
+      await Promise.resolve();
+
+      return {
+        ...payload,
+        client: {
+          ...payload.client,
+          role: 1,
+        },
+      };
+    }
+  );
 }
