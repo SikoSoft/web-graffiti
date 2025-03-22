c5b8a4b694c536626f15940607d941d602d6205a
Aaron Wright <aaron@sikosoft.com>
2025-03-22 19:04:22 +0100

    added dev-watch and dev-sync setup scripts for improving dev experience See #25

---
+++ src/setup/dev-sync.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { Config } from "../server/lib/Config";
+import path from "path";
+import { ConfigProperty } from "../spec/Config";
+
+console.log("sync dev clients");
+
+const main = async () => {
+  const configRoot = process.env.CONFIG_ROOT || "";
+
+  const configFile = path.join(configRoot, "/config.json");
+  const config = await Config.loadConfig(configFile);
+
+  const url = new URL("dev-sync", config[ConfigProperty.WEB_SERVER]);
+
+  await fetch(url.href);
+};
+
+main().catch((error) => {
+  console.error(`Encountered an error in main process: ${error}`);
+});
